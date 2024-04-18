af7feefffd1882f068c2c09891a32ec77bac29e5
Dev2927 <devanand290702@gmail.com>
2024-04-18 21:48:45 +0530

    added a controller & a router

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -18,4 +18,12 @@ app.use(express.urlencoded({ extended: true, limit: limit }));
 app.use(express.static("public"));
 app.use(cookieParser())
 
+
+//Routes
+import userRouter from './routes/user.routes.js'
+
+
+// Routes declaration
+app.use("/api/v1/users", userRouter)
+
 export { app };

7228e985b25583c24881e14a0a52f1c688a46b50
Dev2927 <devanand290702@gmail.com>
2024-04-18 20:45:50 +0530

    Added utils and middleware

---
+++ Backend/src/utils/Cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinaary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // Upload the file on cloudinary
+    const responce = await cloudinary.uploader.upload(localFilePath, {
+      folder: "Vtube",
+      resource_type: "auto",
+    });
+    fs.unlinkSync(localFilePath);
+    return responce;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+const deleteOnCloudinary = async (public_id, resource_type) => {
+  if (!public_id) return null;
+  try {
+    return await cloudinary.uploader.destroy(public_id, {
+      resource_type,
+    });
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export { uploadOnCloudinaary, deleteOnCloudinary };
